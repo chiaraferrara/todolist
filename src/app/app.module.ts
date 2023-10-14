f23e891022bdfdb354515f90b56588a244e69b41
choorywey <123738595+choorywey@users.noreply.github.com>
2023-10-14 22:21:33 +0200

    Firebase set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment.development';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production:false,
+    firebaseConfig : {
+        apiKey: "AIzaSyD8lf86wUWBngDElcwFJq0Y5lU6c0MpkRA",
+        authDomain: "todolist-b19d0.firebaseapp.com",
+        projectId: "todolist-b19d0",
+        storageBucket: "todolist-b19d0.appspot.com",
+        messagingSenderId: "310408811147",
+        appId: "1:310408811147:web:061752534ca77e8b797a8e"
+      }
+};
